7054e400ecbf0499f6f919727a51ca02dc133d1b
Carlos Santos <carlos@gobo.tv>
2017-09-08 10:36:51 +0200

    sept 1

--- public/js/codigo.js
+++
@@ -1,4 +0,0 @@
-window.onscroll = () => {
-  if(this.scrollY <= 120) $("#navbar").removeClass("scroll"); else $("#navbar").addClass("scroll");
-  if(this.scrollY >= 120) $(".nav-item").removeClass("anav"); else $(".nav-item").addClass("anav");
-};
